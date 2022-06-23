f069638f06bff8ab2943cd78dee8be597bb2b651
Abolfazl Jamshidi <jmd.abolfazl14@yahoo.com>
2022-06-23 17:12:34 +0430

    Add product details page

---
+++ src/Pages/ProductPage/Product.js
@@ -0,0 +1,15 @@
+import ProductDetails from "../../Components/ProductDetails/ProductDetails";
+import Newsletter from "../../Components/Newsletter/Newsletter";
+import Footer from "../../Components/Footer/Footer";
+
+const ProductDetailsPage = () => {
+  return (
+    <>
+      <ProductDetails />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+};
+
+export default ProductDetailsPage;
