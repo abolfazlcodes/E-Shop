3f44ea30639072dfdf5cabc518746faf8266d452
Abolfazl Jamshidi <jmd.abolfazl14@yahoo.com>
2022-06-25 09:03:57 +0430

    add login and shopping cart pages

---
+++ src/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import Layout from "../../Layout/Layout";
+import LoginForm from "../../Components/LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+};
+
+export default LoginPage;
