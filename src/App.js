bae4c35ccff32194f8f81a2dfe36426ecea9267f
Abolfazl Jamshidi <jmd.abolfazl14@yahoo.com>
2022-06-27 22:10:54 +0430

    Add routes to app for each page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import routes from "./Routes/routes";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        {routes.map((route) => (
+          <Route {...route} key={route.id} />
+        ))}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Routes/routes.js
@@ -0,0 +1,21 @@
+import HomePage from "../Pages/HomePage/Home";
+import NotFoundPage from "../Components/404Page/NotFoundPage";
+import AllProductsPage from "../Pages/AllProductsPage/AllProductsPage";
+import CheckoutPage from "../Pages/CheckoutPage/CheckoutPage";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import SignupPage from "../Pages/SignupPage/SignupPage";
+import ShoppingCartPage from "../Pages/ShoppingCartPage/ShoppingCartPage";
+import Product from "../Pages/ProductPage/Product";
+
+const routes = [
+  { id: 1, element: <HomePage />, path: "/" },
+  { id: 2, element: <NotFoundPage />, path: "*" },
+  { id: 3, element: <AllProductsPage />, path: "/products" },
+  { id: 4, element: <CheckoutPage />, path: "/checkout" },
+  { id: 5, element: <LoginPage />, path: "/login" },
+  { id: 6, element: <SignupPage />, path: "/signup" },
+  { id: 7, element: <ShoppingCartPage />, path: "/cart" },
+  { id: 8, element: <Product />, path: "/product/:id" },
+];
+
+export default routes;
